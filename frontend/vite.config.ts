fb762f24b59e448f51938b179e402aefbd988394
Patrick <PatrickNeira@pm.me>
2024-02-14 18:04:03 +0000

    changed configs

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,10 +4,18 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  root: ".",
+  mode: "development",
+  server:{
+    port:3000,
+    host:"0.0.0.0",
+    open:true,
+    strictPort:true,
+    https:false
+    
+
+  },
   build: {
     ssr: false,
-    watch: {
-      include: ['src/**',"src/components/**"]
-    }
   }
 })
