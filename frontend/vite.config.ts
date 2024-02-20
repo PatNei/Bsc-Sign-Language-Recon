8c60d2f70a32142404ed3aa33e6dc21b144c0073
Patrick <PatrickNeira@pm.me>
2024-02-20 14:38:56 +0000

    add vite dev settings

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,21 +1,26 @@
-import { defineConfig } from 'vite'
+import { UserConfig, defineConfig,loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  root: ".",
-  mode: "development",
-  server:{
-    port:3000,
-    host:"0.0.0.0",
-    open:true,
-    strictPort:true,
-    https:false
-    
+export default ({mode}:UserConfig) => { 
+  process.env = {...process.env, ...loadEnv(mode, process.cwd())};
+  console.log(process.env.VITE_HOST_IP)
+  return defineConfig({
+    plugins: [react()],
+    root: ".",
+    mode: "development",
+    server:{
+      port:3000,
+      open:true,
+      strictPort:true,
+      hmr: {
+        host: process.env.VITE_HOST_IP ?? "localhost"
+      }
+      
 
-  },
-  build: {
-    ssr: false,
-  }
-})
+    },
+    build: {
+      ssr: false,
+    }
+  })
+}
