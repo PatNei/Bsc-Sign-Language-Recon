6093b4dd8871d2e787a987df52826bab4cc45e5c
siar <siar@itu.dk>
2024-04-16 13:07:43 +0200

    Restart clip

--- frontend/src/components/Recognizer.tsx
+++ frontend/src/components/Recognizer.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import Canvas from "./Canvas";
 import { LetterChallenge, WordChallenge } from "../challenges";
 import DynamicCanvas from "./DynamicCanvas";
+import { JSX } from "react/jsx-runtime";
 
 interface props {
   challenges: LetterChallenge[] | WordChallenge[];
@@ -12,10 +13,25 @@ export default function Recognizer({ challenges, dynamic }: props) {
   const [boolski, setBoolski] = useState<boolean>(false);
   const [i, setI] = useState(0);
   const [[sign, signSrc], setChallenge] = useState(challenges[i]);
+  const [key, setKey] = useState(0);
   const [srcIndex, setSrcIndex] = useState(0);
   const [letterCounter, setLetterCounter] = useState(0);
   const [shouldCapture, setShouldCapture] = useState<boolean>(false);
   const [response, setResponse] = useState<string>("");
+  const [currentSrc, setCurrentSrc] = useState<JSX.Element>(
+    <iframe
+      className="pb-2"
+      width="420"
+      height="315"
+      src={`https://www.youtube.com/embed/${
+        signSrc[0].split(":")[0]
+      }?start=${Math.floor(
+        +signSrc[0].split(":")[1] / 1000
+      )}&cc_load_policy=1&autoplay=1&end=${
+        Math.floor(+signSrc[0].split(":")[1] / 1000) + 1
+      }&loop=1&fs=0&iv_load_policy=3`}
+    />
+  );
 
   if (response === sign) {
     if (!boolski) {
@@ -37,6 +53,7 @@ export default function Recognizer({ challenges, dynamic }: props) {
   if (i >= challenges.length) {
     return <div>"Well" done!</div>;
   }
+
   return (
     <div className="w-full h-full flex flex-col items-center">
       <div className="w-full h-full flex flex-col items-center">
@@ -49,26 +66,67 @@ export default function Recognizer({ challenges, dynamic }: props) {
             />
           ) : (
             <div className="grid grid-cols-1">
-              <iframe
-                className="pb-2"
-                key={i}
-                width="420"
-                height="315"
-                src={`https://www.youtube.com/embed/${
-                  signSrc[srcIndex].split(":")[0]
-                }?start=${Math.floor(
-                  +signSrc[srcIndex].split(":")[1] / 1000
-                )}&cc_load_policy=1&autoplay=1&end=${
-                  Math.floor(+signSrc[srcIndex].split(":")[1] / 1000) + 1
-                }&loop=1&fs=0&iv_load_policy=3`}
-              />
-              <button
-                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-                type="button"
-                onClick={() => setSrcIndex((i) => (i + 1) % signSrc.length)}
-              >
-                Next Clip {`(${srcIndex + 1}/${signSrc.length})`}
-              </button>
+              {currentSrc}
+              <div className="flex justify-evenly">
+                <div>
+                  <button
+                    className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                    type="button"
+                    onClick={(_) => {
+                      let newSrcIndex = (srcIndex + 1) % signSrc.length;
+                      setCurrentSrc((_) => (
+                        <iframe
+                          key={0}
+                          className="pb-2"
+                          width="420"
+                          height="315"
+                          src={`https://www.youtube.com/embed/${
+                            signSrc[newSrcIndex].split(":")[0]
+                          }?start=${Math.floor(
+                            +signSrc[newSrcIndex].split(":")[1] / 1000
+                          )}&cc_load_policy=1&autoplay=1&end=${
+                            Math.floor(
+                              +signSrc[newSrcIndex].split(":")[1] / 1000
+                            ) + 1
+                          }&loop=1&fs=0&iv_load_policy=3`}
+                        />
+                      ));
+                      setSrcIndex(newSrcIndex);
+                      setKey(0);
+                    }}
+                  >
+                    Next Clip {`(${srcIndex + 1}/${signSrc.length})`}
+                  </button>
+                </div>
+                <div>
+                  <button
+                    onClick={(_) => {
+                      setCurrentSrc((_) => (
+                        <iframe
+                          key={key + 1}
+                          className="pb-2"
+                          width="420"
+                          height="315"
+                          src={`https://www.youtube.com/embed/${
+                            signSrc[srcIndex].split(":")[0]
+                          }?start=${Math.floor(
+                            +signSrc[srcIndex].split(":")[1] / 1000
+                          )}&cc_load_policy=1&autoplay=1&end=${
+                            Math.floor(
+                              +signSrc[srcIndex].split(":")[1] / 1000
+                            ) + 1
+                          }&loop=1&fs=0&iv_load_policy=3`}
+                        />
+                      ));
+                      setKey((i) => i + 1);
+                    }}
+                    className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                    type="button"
+                  >
+                    ↺
+                  </button>
+                </div>
+              </div>
             </div>
           )}
           <p className="pt-2">{`${sign[0].toUpperCase()}${sign.slice(1)}`}</p>
@@ -90,7 +148,7 @@ export default function Recognizer({ challenges, dynamic }: props) {
           )}
         </p>
       </div>
-      <button type="button" onClick={() => setShouldCapture(!shouldCapture)}>
+      <button type="button" onClick={(_) => setShouldCapture(!shouldCapture)}>
         {shouldCapture ? "Disable" : "Enable"} camera
       </button>
     </div>
