40e98f454076e2fdd337316bcc48d2d26494d2b7
siar <siar@itu.dk>
2024-04-23 15:16:34 +0200

    Don't include buttons in common words

--- frontend/src/components/Recognizer.tsx
+++ frontend/src/components/Recognizer.tsx
@@ -103,6 +103,12 @@ export default function Recognizer({ challenges, dynamic }: props) {
                       }}
                     >
                       Previous Clip
+                    </button>
+                  </div>
+                  <div>
+                    <button
+                      className={
+                        srcIndex + 1 < signSrc.length
                           ? "bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
                           : "bg-blue-500 text-white font-bold py-2 px-4 rounded opacity-50 cursor-not-allowed"
                       }
