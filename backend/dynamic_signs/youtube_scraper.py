08fdf70287e97c34cf201d2dfd87df7422aaf0bc
siar <siar@itu.dk>
2024-04-25 16:14:27 +0200

    Fix logging

--- backend/dynamic_signs/youtube_scraper.py
+++ backend/dynamic_signs/youtube_scraper.py
@@ -66,6 +66,7 @@ class YouTubeScraper():
                     break
                 
                 yt = YouTube(f"https://www.youtube.com/watch?v={video_id}")
+                title = yt.title.lower()
                         
                 try:        
                     yt.bypass_age_gate()
@@ -165,8 +166,9 @@ class YouTubeScraper():
 
 if __name__ == "__main__":
     yt = YouTubeScraper(Path("dynamic_signs/common_words_new.csv"), Path("dynamic_signs/video_ids.txt"), num_hands=2)
-    yt.find_common_words(min_occurances=100, max=0)
-    yt.get_video_signs(max=0, out_path="youtube_with_asl_in_title.csv",seconds_per_clip=1)
+    # yt.find_common_words(min_occurances=100, max=0)
+    yt.extract_captions(max=0, only_common_words=False)
+    # yt.get_video_signs(max=0, out_path="youtube_with_asl_in_title.csv",seconds_per_clip=1)
     # yt.get_video_signs(max=0,seconds_per_clip=1)
     # yt.find_common_words(min_occurances=50, max=0)
     # yt.find_common_words(min_occurances=0, max=20)
