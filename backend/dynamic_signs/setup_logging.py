7208e49912bfeb4d06c441110ab5bffdb1487244
Patrick <PatrickNeira@pm.me>
2024-05-07 10:18:57 +0000

    make optimising model optional and fix logging paths

--- backend/dynamic_signs/setup_logging.py
+++ backend/dynamic_signs/setup_logging.py
@@ -4,7 +4,7 @@ import os
 from pathlib import Path
 
 
-def setup_logging(path):
+def setup_logging(path,current_date = datetime.datetime.now()):
     # Setup Logging
     BASE_PATH = Path().cwd().joinpath("./logs")
     if not os.path.exists(BASE_PATH):
@@ -14,11 +14,11 @@ def setup_logging(path):
     if not os.path.exists(BASE_PATH):
         os.mkdir(BASE_PATH)
         
-    CURRENT_DATE = datetime.datetime.now().strftime("%d-%m-%Y")
+    CURRENT_DATE = current_date.strftime("%d-%m-%Y")
     BASE_PATH = BASE_PATH.joinpath(CURRENT_DATE)
     if not os.path.exists(BASE_PATH):
         os.mkdir(BASE_PATH)
 
-    CURRENT_TIME = datetime.datetime.now().strftime("%d-%m-%Y(%H-%M-%S)")
+    CURRENT_TIME = current_date.strftime("%d-%m-%Y(%H-%M-%S)")
     logging.basicConfig(filename=BASE_PATH.joinpath(f"{CURRENT_TIME}.log"),level=logging.INFO)
         
--- backend/dynamic_signs/train_scripts/train_model.py
+++ backend/dynamic_signs/train_scripts/train_model.py
@@ -16,6 +16,7 @@ from sklearn.base import BaseEstimator
 from sklearn.model_selection import RandomizedSearchCV, cross_val_score
 from sklearn.pipeline import make_pipeline
 from sklearn.svm import SVC
+from dynamic_signs.setup_logging import setup_logging
 from sign.training.load_data.dynamic_loader import DynamicLoader
 from sign.CONST import AMOUNT_OF_KEYFRAMES
 from collections import Counter
@@ -31,19 +32,20 @@ class EK(Enum):
     BCLR = "bclr"
 
 # Setup Logging
-CURRENT_DATE = datetime.datetime.now().strftime("%d-%m-%Y")
+CURRENT_DATE = datetime.datetime.now()
+CURRENT_DATE_date_str = datetime.datetime.now().strftime("%d-%m-%Y")
+CURRENT_DATE_time_str = datetime.datetime.now().strftime("%d-%m-%Y(%H-%M-%S)")
 RANDOM_STATE = 422
-TRAINING_PATH = "model_output"
+TRAINING_PATH = "trained_models"
+
 if not os.path.exists(TRAINING_PATH):
     os.mkdir(TRAINING_PATH)
-BASE_PATH = TRAINING_PATH + "/" +CURRENT_DATE
+
+setup_logging(TRAINING_PATH,CURRENT_DATE)
+
+BASE_PATH = TRAINING_PATH + "/" + CURRENT_DATE_date_str
 if not os.path.exists(BASE_PATH):
     os.mkdir(BASE_PATH)
-LOG_PATH = Path().cwd().joinpath(f"{BASE_PATH}/logs")
-if not os.path.exists(LOG_PATH):
-    os.mkdir(LOG_PATH)
-CURRENT_TIME = datetime.datetime.now().strftime("%d-%m-%Y(%H-%M-%S)")
-logging.basicConfig(filename=LOG_PATH.joinpath(f"{CURRENT_TIME}.log"),level=logging.INFO)
     
 
 def load_csv(CSV_PATH:Path,AMOUNT_OF_KEYFRAMES=AMOUNT_OF_KEYFRAMES,NUM_HANDS=2):
@@ -68,7 +70,7 @@ def get_parameters(name:EK):
         return {"voting":["hard","soft"]}
     
     
-def get_base_estimators(name:EK,xs:np.ndarray,ys:tuple[str]) -> BaseEstimator:
+def get_base_estimators(name:EK,xs:np.ndarray,ys:tuple[str],optimised=False) -> BaseEstimator:
     if name == EK.LR:
         return LogisticRegression(random_state=RANDOM_STATE,max_iter=10000)
     if name == EK.RF:
@@ -76,26 +78,30 @@ def get_base_estimators(name:EK,xs:np.ndarray,ys:tuple[str]) -> BaseEstimator:
     if name == EK.SVM or name == EK.SVC:
         return SVC(probability=name==EK.SVC) 
     if name == EK.BCLR:
-        be = find_optimized_model(EK.LR,xs,ys)
+        be = get_model(EK.LR,xs,ys,optimised)
         return BaggingClassifier(be)
     if name == EK.VC:
-        lr = find_optimized_model(EK.LR,xs,ys)
-        bclr = find_optimized_model(EK.BCLR,xs,ys)
-        rf = find_optimized_model(EK.RF,xs,ys)
+        lr = get_model(EK.LR,xs,ys,optimised)
+        bclr = get_model(EK.BCLR,xs,ys,optimised)
+        rf = get_model(EK.RF,xs,ys,optimised)
         return VotingClassifier( estimators=[ 
                                                 ('lr', lr), 
                                                 ('rf', rf), 
                                                 ('bclr', bclr)
         ]) 
 
-def find_optimized_model(estimator_name:EK,xs:np.ndarray,ys:tuple[str],n_jobs=-1):
-    estimator = get_base_estimators(estimator_name,xs,ys)
-    parameters = get_parameters(estimator_name)
-    pre_clf = RandomizedSearchCV(estimator,parameters,n_jobs=n_jobs,verbose=2,random_state=RANDOM_STATE) # type: ignore
-    logging.info("Searching for best hyper parameters")
+def get_model(estimator_name:EK,xs:np.ndarray,ys:tuple[str],optimised=False,n_jobs=-1):
+    estimator = get_base_estimators(estimator_name,xs,ys,optimised)
+    pre_clf:BaseEstimator
+    if optimised:
+        parameters = get_parameters(estimator_name)
+        pre_clf = RandomizedSearchCV(estimator,parameters,n_jobs=n_jobs,verbose=2,random_state=RANDOM_STATE) # type: ignore
+        logging.info("Returning model with best hyper parameters")
+    else:
+        pre_clf = make_pipeline(estimator)
+        logging.info("Returning model with default parameters")
     pre_clf.fit(xs,ys)
-    return pre_clf
-
+    return pre_clf    
 
 def evaluate_model(clf:BaseEstimator,xs:np.ndarray,ys:tuple[str],cv=3):
     cross_val_score(clf, xs, ys, cv=cv, scoring="accuracy")
@@ -105,7 +111,6 @@ def main():
     parser.add_argument("input", 
                         help="The .csv containing the input data.",
                         type=str)
-
     parser.add_argument("model",
                         choices=[x.value for x in EK],
                         help="The model that you wish to use",
@@ -113,6 +118,10 @@ def main():
     parser.add_argument("--scale","-s",
                         help="Should the input be scaled ?",
                         dest="scale",
+                        action='store_true')
+    parser.add_argument("--optimise","-o",
+                        help="Should we find the most optimised?",
+                        dest="optimise",
                         action='store_true')
     parser.add_argument("--jobs","-j",
                         help="How many cores should we use?",
@@ -129,10 +138,11 @@ def main():
 
     input_path = Path(args.input)
     model_type = EK(args.model)
-    should_scale = args.scale
+    should_scale:bool = args.scale
+    optimise:bool = args.optimise
     out_path:Path
     if args.out == None:
-        out_path = Path(f"{BASE_PATH}/dynamic-{model_type}-{CURRENT_TIME}.joblib")
+        out_path = Path(f"{BASE_PATH}/dynamic-{model_type}-{CURRENT_DATE_time_str}.joblib")
     else: 
         out_path = Path(args.out)
         out_path = out_path.joinpath(".joblib") if out_path.suffix != '.joblib' else out_path
@@ -140,16 +150,17 @@ def main():
     if not input_path.exists() or input_path.suffix != '.csv':
         raise ValueError(f"Input file must be a .csv :thinking:")
 
-    if not out_path.exists():
-        logging.info(f"Couldn't find {out_path.name}, so created it.")
-        out_path.touch()
-
     logging.info(f"About to process {input_path} outputting to {out_path}")
     
     xs,ys,xs_test,ys_test = load_csv(input_path)
-    n_jobs = args.n_jobs
+    
+    n_jobs:int = args.n_jobs
     logging.info(f"training set: { Counter(ys) }")
     logging.info(f"test set: { Counter(ys_test) }")
+    
+    if not should_scale and model_type == EK.SVC or model_type == EK.SVM:
+        logging.warn(f"{model_type.value} benefit from scaling, it might be good to rerun with the -s or --scale flag")
+        
     if should_scale:
         if model_type == EK.RF:
             logging.warn("Random forrest doesn't benefit from scaling the data, it might be beneficial to remove the -s or --scale flag.")
@@ -160,26 +171,33 @@ def main():
         if isinstance(xs_test,spmatrix) or isinstance(xs,spmatrix):
             exit()
     
-    best_clf = find_optimized_model(model_type,xs,ys,n_jobs)
-    logging.info(f"Best Estimator: {best_clf.best_estimator_}")
-    logging.info(f"Best Index: {best_clf.best_index_}")
-    logging.info(f"Best Params: {best_clf.best_params_}")
-    logging.info(f"Best Score: {best_clf.best_score_}")
-
-    y_pred = best_clf.predict(xs_test)
+    clf = get_model(model_type,xs,ys,optimise,n_jobs)
     
-    # logging.info(xs.shape)
-    # logging.info(xs_test.shape)
-    logging.info(f"Cross val score:\n{cross_val_score(best_clf, xs, ys, cv=3, scoring='accuracy')}")
-    logging.info(f"Classification Report for training set:\n{classification_report(ys,cross_val_predict(best_clf,xs,ys))}")
+    if isinstance(clf,RandomizedSearchCV):
+        logging.info("- Search resulted in -")
+        logging.info(f"Best Estimator: {clf.best_estimator_}")
+        logging.info(f"Best Index: {clf.best_index_}")
+        logging.info(f"Best Params: {clf.best_params_}")
+        logging.info(f"Best Score: {clf.best_score_}")
+
+    y_pred = clf.predict(xs_test)
+    
+    logging.info(f"Cross val score:\n{cross_val_score(clf, xs, ys, cv=3, scoring='accuracy')}")
+    logging.info(f"Classification Report for training set:\n{classification_report(ys,cross_val_predict(clf,xs,ys))}")
     logging.info(f"Classification Report for test set:\n{classification_report(ys_test,y_pred,digits=4,zero_division=1)}")
+    logging.info(f"Probabilities:\n{[(x,y) for (x,y) in zip(ys_test,clf.predict_proba(xs_test))]}")
+    
     cm = confusion_matrix(ys_test,y_pred)
-    display = ConfusionMatrixDisplay(cm,display_labels=best_clf.classes_)
+    display = ConfusionMatrixDisplay(cm,display_labels=clf.classes_)
     display.plot()
-    matplotlib.pyplot.savefig(f"{LOG_PATH}/cm-{CURRENT_TIME}")
-    #logging.info(f"Probabilities:\n{[(x,y) for (x,y) in zip(ys_test,best_clf.predict_proba(xs_test))]}")
+    matplotlib.pyplot.savefig(f"{BASE_PATH}/cm-{CURRENT_DATE_time_str}")
+    
+    if not out_path.exists():
+        logging.info(f"Couldn't find {out_path.name}, so created it.")
+        out_path.touch()
+
     from joblib import dump
-    dump(best_clf, out_path)
+    dump(clf, out_path)
 
 if __name__ == "__main__":
   main()
