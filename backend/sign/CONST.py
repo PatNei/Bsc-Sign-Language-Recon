bb5d425ed8ff6e7168261cae84e762081f8729bf
tjobi <tobias@brund.info>
2024-05-14 20:29:26 +0200

    rename dynamic and change const path

--- backend/sign/CONST.py
+++ backend/sign/CONST.py
@@ -4,7 +4,7 @@ TRAIN_PATH = 'model/train.csv'
 STATIC_MODEL_PATH = 'model/static_model.joblib'
 STATIC_MODEL_USES_HANDEDNESS = False #REMEMBER TO UPDATE THIS ONE WITH STATIC_MODEL_PATH
 
-DYNAMIC_MODEL_PATH = 'model/DYN_JZ_M1.joblib'
+DYNAMIC_MODEL_PATH = 'model/dynamic_best/random_forrest/dynamic-RF.joblib'
 CLASSIFIER_PATH = "model/keypoint_classifier_label.csv"
 AMOUNT_OF_KEYFRAMES = 24
 
